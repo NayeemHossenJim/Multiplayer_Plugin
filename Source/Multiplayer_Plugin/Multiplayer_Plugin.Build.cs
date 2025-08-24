3d89363b380d5e5cfd3211f5f8df948d2a4d22ad
NayeemHossenJim <nayeemhossenjim@gmail.com>
2025-08-25 01:09:30 +0600

    Update build file

--- Source/Multiplayer_Plugin/Multiplayer_Plugin.Build.cs
+++ Source/Multiplayer_Plugin/Multiplayer_Plugin.Build.cs
@@ -8,6 +8,6 @@ public class Multiplayer_Plugin : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "OnlineSubsystem", "OnlineSubsystemSteam" });
 	}
 }
